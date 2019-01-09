bb6e81ec0fc6619bed401cec9b014fe87d4025d9
wongalofonso <alfonsowongsd@gmail.com>
2019-01-09 00:32:24 -0800

    added webpack file

---
+++ webpack.common.js
@@ -0,0 +1,56 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    javascript: './src/js/index.jsx'
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, 'dist')
+  },
+  devtool: 'source-map',
+  resolve: {
+    alias: {
+      react: path.join(__dirname, 'node_modules', 'react')
+    },
+    extensions: ['.js', '.jsx']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: { loader: 'babel-loader' }
+      },
+      {
+        test: /\.html$/,
+        loader: 'file?name=[name].[ext]'
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader' },
+          { loader: 'sass-loader' }
+        ]
+      },
+      {
+        test: /\.(png|jpg)$/,
+        use: { loader: 'file-loader' }
+      },
+      {
+        test: /\.mp4$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]'
+          }
+        }]
+      },
+      {
+        test: /\.pdf$/,
+        loader: 'file?name=[name].[ext]'
+      }
+    ],
+  }
+}
