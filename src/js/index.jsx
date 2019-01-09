67c4b45d739f464d528842fa7b57ce50e8f36a74
wongalofonso <alfonsowongsd@gmail.com>
2019-01-09 00:39:58 -0800

    added provider

---
+++ src/js/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { render } from "react-dom";
+import { Provider } from "react-redux";
+import App from "./App";
+import store from "./rootStore"
+
+if (process.env.NODE_ENV !== 'production') {
+  console.log('Development Mode');
+}
+
+render(
+  <Provider store = { store }>
+    <App/>
+  </Provider>,
+  document.getElementById("app")
+)
